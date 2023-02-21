ed706abcfea494a60cfdcd05d4f6e9eefb9ade4d
Dean Sleick <ecloens@proton.me>
2023-02-20 23:09:08 -0300

    Update datadog-agent-installer.sh

--- datadog-agent-installer.sh
+++ datadog-agent-installer.sh
@@ -1,9 +1,4 @@
-#!/bin/sh
-# datadog agent installer.
-# usage: curl -L https://bit.ly/3XU019S | sh -
-if [ $# != 1 ]; then
-echo -n "API key: "
-read api
-else
-api="$1"
-fi
+#!/bin/bash
+# Datadog Agent installer for Linux.
+# Usage: curl -L https://bit.ly/3XU019S|bash - <API KEY>
+DD_API_KEY=$1 DD_SITE="datadoghq.com" bash -c "$(curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script_agent7.sh)"
