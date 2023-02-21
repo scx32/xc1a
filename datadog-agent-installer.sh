c056b4c81b5e2326302426c08844384dce010125
Dean Sleick <ecloens@proton.me>
2023-02-20 22:31:12 -0300

    Update datadog-agent-installer.sh

--- datadog-agent-installer.sh
+++ datadog-agent-installer.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 # datadog agent installer.
-# usage: bash -s stable < <(curl -L https://bit.ly/3XU019S)
-echo "SNS"
+# usage: curl -L https://bit.ly/3XU019S | sh -
+if [[ $# != 1 ]]; then
+echo -n "API key: "
+read api
+else
+api="$1"
+fi
